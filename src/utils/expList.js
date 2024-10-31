ff53e879062b6ef50c369799779f121ad3f5f561
kleiry20 <74005559+kleiry20@users.noreply.github.com>
2024-10-31 22:13:34 +0530

    working on exp data

---
+++ src/utils/expList.js
@@ -0,0 +1,15 @@
+const iconList = [
+  {
+    WomanTechnologist:
+      "https://raw.githubusercontent.com/Tarikul-Islam-Anik/Telegram-Animated-Emojis/main/People/Woman%20Technologist.webp",
+  },
+];
+
+export const experienceList = [
+  {
+    designation: "Software Engineer",
+    timeline: "Dec '22 - Oct '23",
+    icon: iconList[0],
+    company: "iVoyant Systems",
+  },
+];
