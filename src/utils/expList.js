912b9bd2c8ab10e479cd00d9fafa9ee6c818f7f1
kleiry20 <74005559+kleiry20@users.noreply.github.com>
2024-11-07 23:17:18 +0530

    revamping

--- src/utils/expList.js
+++ src/utils/expList.js
@@ -12,10 +12,10 @@ export const experienceList = [
     icon: iconList[0],
     company: "iVoyant Systems",
   },
-  // {
-  //   designation: "Web Development Intern",
-  //   timeline: "Dec '22 - Oct '23",
-  //   icon: iconList[0],
-  //   company: "Leanpitch Technologies",
-  // },
+  {
+    designation: "Web Development Intern",
+    timeline: "Dec '22 - Oct '23",
+    icon: iconList[0],
+    company: "Leanpitch Technologies",
+  },
 ];
